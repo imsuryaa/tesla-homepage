4df31e9101fea2ec8e4edf266ee45da393d73468
suryatejaj97 <suryatejaj97@gmail.com>
2021-05-05 13:56:36 +0530

    minor changes

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import './Button.css'
 
-const Button = () => {
+const Button = ({imp, text, link}) => {
     return (
-        <div className='button'>
-            
-        </div>
+        <div className={`button ${imp === 'secondary' ? 'button__white' : '' }`}>
+            <a href={link}>
+                {text}
+            </a>
+        </div> 
     )
 }
 
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,10 +1,9 @@
 import React from 'react'
 import Button from './Button'
 import './Item.css'
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
 
 const Item = ({title, desc, backgroundImg, leftBtnTxt, leftBtnLink,
-    rightBtnTxt, rightBtnLink, twoButtons, first}) => {
+    rightBtnTxt, rightBtnLink, twoButtons}) => {
     return (
         <div className='item' style={{
             backgroundImage: `url(${backgroundImg})`
@@ -20,14 +19,9 @@ const Item = ({title, desc, backgroundImg, leftBtnTxt, leftBtnLink,
                     <div className='item__button'>
                         <Button imp='primary' text={leftBtnTxt} link={leftBtnLink} />
                         {twoButtons && (
-                            <Button imp='secondary' text={rightBtnTxt} link={rightBtnTxt} />
+                            <Button imp='secondary' text={rightBtnTxt} link={rightBtnLink} />
                         )}
                     </div>
-                    {first && (
-                        <div className='item__expand'>
-                            <ExpandMoreIcon />
-                        </div>
-                    )}
                 </div>
             </div>
         </div>
